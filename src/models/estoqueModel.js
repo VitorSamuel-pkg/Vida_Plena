2bdcdf98e05ba4af1035bb6a8425fb0aefccfcb2
unknown <vso.contatovitorsamuel@gmail.com>
2025-06-21 14:25:13 -0300

    Finalizado estoqueModel.js

---
+++ src/models/estoqueModel.js
@@ -0,0 +1,35 @@
+const { sequelize } = require("../config/db");
+const { DataTypes } = require('sequelize');
+
+const { produtoModel } = require('./produtoModel');
+
+const estoqueModel = sequelize.define('Estoque', {
+    ID_Estoque:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    qtdEntradaProduto:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    qtdSaidoProduto:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    idEstoqueProduto:{
+        type: DataTypes.INTEGER,
+        references:{
+            model: produtoModel,
+            key: 'ID_Produto'
+        },
+        allowNull: false
+    }
+
+},{
+     tableName: 'Estoque',
+     timestamps: false
+});
+
+produtoModel.hasMany(estoqueModel, {foreignKey: 'idEstoqueProduto', as: 'Produto'});
+estoqueModel.belongsToMany(produtoModel, {foreignKey: 'idEstoqueProduto', as: 'Produto'});
