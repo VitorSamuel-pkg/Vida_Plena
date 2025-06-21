14d42049b78c879bfde3e94de0492e791c1a2f90
unknown <vso.contatovitorsamuel@gmail.com>
2025-06-21 15:11:34 -0300

    socorro

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,22 @@
+const parseDateBd = (dataString) => {
+    let [ano, mes, dia] = dataString.split('-');
+    ano = parseInt(ano);
+    mes = parseInt(mes);
+    dia = parseInt(dia);
+    
+    return new Date(ano, mes - 1, dia + 1);
+}
+
+const parseTimeBd = (time) => {
+    let horas = parseInt(time.getHours()) + 3;
+    let minutos = parseInt(time.getMinutes());
+    let segundos = parseInt(time.getSeconds());
+
+    horas = horas < 10? '0' + horas : horas;
+    minutos = minutos < 10? '0' + minutos : minutos;
+    segundos = segundos < 10? '0' + segundos : segundos;    
+
+    return `${horas}:${minutos}:${segundos}`
+}        
+
+module.exports = {parseDateBd, parseTimeBd};
